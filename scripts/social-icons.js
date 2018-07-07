a8d5c6b7c3c505c5cfaf7fce028d5ccc1b821550
TheCakeDevourer <thecakedevourer@gmail.com>
2018-07-07 19:08:06 +0100

    Fixed a bug in social-icons.js

--- scripts/social-icons.js
+++ scripts/social-icons.js
@@ -1,9 +1,9 @@
 function hoverIcon(n) {
-  n.querySelector('img').src = '/images/icons/' + n.getAttribute('icon'); + '-yellow.png';
+  n.querySelector('img').src = '/images/icons/' + n.getAttribute('icon') + '-yellow.png';
 }
 
 function unHover(n) {
-  n.querySelector('img').src = '/images/icons/' + n.getAttribute('icon'); + '-black.png';
+  n.querySelector('img').src = '/images/icons/' + n.getAttribute('icon') + '-black.png';
 }
 
 // This code was made by thecakedevourer, can be used by other people but credits must be given.
