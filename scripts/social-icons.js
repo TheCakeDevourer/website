f2fcfb3d341d4c20e53f202a66a2de23ec57a260
TheCakeDevourer <thecakedevourer@gmail.com>
2018-07-07 19:05:59 +0100

    Update social-icons.js

--- scripts/social-icons.js
+++ scripts/social-icons.js
@@ -5,3 +5,5 @@ function hoverIcon(n) {
 function unHover(n) {
   n.querySelector('img').src = '/images/icons/' + n.getAttribute('icon'); + '-black.png';
 }
+
+// This code was made by thecakedevourer, can be used by other people but credits must be given.
